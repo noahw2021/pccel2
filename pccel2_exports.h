a0ca8170703711d600745b4afceff4c2edc4ce5a
Noah Wooten <noah.wooten@icloud.com>
2023-12-24 15:48:47 -0500

    create pccel2 headers

---
+++ pccel2_exports.h
@@ -0,0 +1,12 @@
+//
+//  pccel2_exports.h
+//  pccel2
+//
+//  Created by Noah Wooten on 12/24/23.
+//
+
+#ifndef pccel2_exports_h
+#define pccel2_exports_h
+
+
+#endif /* pccel2_exports_h */
---
+++ types.h
@@ -0,0 +1,12 @@
+//
+//  types.h
+//  pccel2
+//
+//  Created by Noah Wooten on 12/24/23.
+//
+
+#ifndef types_h
+#define types_h
+
+
+#endif /* types_h */
