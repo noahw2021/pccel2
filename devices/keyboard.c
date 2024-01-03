1f306c783b720194fb13b412f0196bc4f3cf829f
Noah Wooten <noah.wooten@icloud.com>
2024-01-03 15:30:24 -0500

    create keyboard definitions

--- devices/keyboard.c
+++ devices/keyboard.c
@@ -6,3 +6,31 @@
 //
 
 #include "devices.h"
+
+void KeyboardInit(void) {
+    return;
+}
+
+void KeyboardShutdown(void) {
+    return;
+}
+
+void KeyboardSetKeyDownInterrupt(WORD64 Interrupt) {
+    return;
+}
+
+void KeyboardSetKeyUpInterrupt(WORD64 Interrupt) {
+    return;
+}
+
+WORD64 KeyboardGetKeyMapPointer(void) {
+    return 0x00;
+}
+
+void KeyboardHandleKeyUp(BYTE ScanCode) {
+    return;
+}
+
+void KeyboardHandleKeyDown(BYTE ScanCode) {
+    return;
+}
