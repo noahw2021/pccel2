37e01e079921907bef1aae479dbe82ed2d5e806e
Noah Wooten <noah.wooten@icloud.com>
2024-01-05 15:53:27 -0500

    finish io stuff

--- ini/ini_io.c
+++ ini/ini_io.c
@@ -37,16 +37,70 @@ void IniCreateField(
         File->Entries = realloc(File->Entries,
             sizeof(INI_ENTRY) * (File->EntryCount + 1));
     }
+    
+    PINI_ENTRY NewEntry = &File->Entries[File->EntryCount];
+    File->EntryCount++;
+    
+    strcpy(NewEntry->Name, FieldName);
+    strcpy(NewEntry->Value, Default);
+    NewEntry->Numerical = strtoull(NewEntry->Value, NULL, 10);
+    
+    return;
 }
 
 PINI_ENTRY IniReadField(PINI_FILE File, const char* FieldName) {
+    if (!File)
+        return NULL;
+    
+    for (int i = 0; i < File->EntryCount; i++) {
+        PINI_ENTRY ThisEntry = &File->Entries[i];
+        if (!strcmp(ThisEntry->Name, FieldName))
+            return ThisEntry;
+    }
+    
     return NULL;
 }
 
 void IniWriteField(PINI_FILE File, PINI_ENTRY Entry) {
+    for (int i = 0; i < File->EntryCount; i++) {
+        PINI_ENTRY ThisEntry = &File->Entries[i];
+        if (!strcmp(ThisEntry->Name, Entry->Name)) {
+            memcpy(ThisEntry, Entry, sizeof(INI_ENTRY));
+            break;
+        }
+    }
+    
     return;
 }
 
 void IniDeleteField(PINI_FILE File, const char* FieldName) {
+    if (!File)
+        return;
+    
+    BYTE DidDeleteOccur = 0;
+    PINI_ENTRY NewEntryList = malloc(sizeof(INI_ENTRY) *
+        File->EntryCount - 1);
+    
+    int c = 0;
+    for (int i = 0; i < File->EntryCount; i++) {
+        PINI_ENTRY ThisEntry = &File->Entries[i];
+        
+        if (!strcmp(ThisEntry->Name, FieldName)) {
+            DidDeleteOccur = 1;
+            continue;
+        } else {
+            memcpy(&NewEntryList[c], ThisEntry, 
+                sizeof(INI_ENTRY));
+            c++;
+        }
+    }
+    
+    if (DidDeleteOccur) {
+        free(File->Entries);
+        File->Entries = NewEntryList;
+    } else {
+        free(NewEntryList);
+    }
+    
     return;
 }
