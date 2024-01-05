866ba025e461b2b2ee5e345777e65547d7c6b213
Noah Wooten <noah.wooten@icloud.com>
2024-01-05 15:45:39 -0500

    create io function definitions

--- ini/ini_io.c
+++ ini/ini_io.c
@@ -5,4 +5,27 @@
 //  Created by Noah Wooten on 1/5/24.
 //
 
-#include <stdio.h>
+#include "ini.h"
+
+BYTE IniDoesFieldExist(PINI_FILE File, const char* FieldName) {
+    return 0;
+}
+
+void IniCreateField(
+    PINI_FILE File, const char* FieldName,
+    const char* Default
+) {
+    return;
+}
+
+PINI_ENTRY IniReadField(PINI_FILE File, const char* FieldName) {
+    return NULL;
+}
+
+void IniWriteField(PINI_FILE File, PINI_ENTRY Entry) {
+    return;
+}
+
+void IniDeleteField(PINI_FILE File, const char* FieldName) {
+    return;
+}
