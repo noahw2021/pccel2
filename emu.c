18ca249ae8bab4fd58968ca281b0b9d1c4d94e4a
Noah Wooten <noah.wooten@icloud.com>
2024-01-05 16:07:10 -0500

    create emulator header

--- emu.c
+++ emu.c
@@ -6,4 +6,49 @@
 //
 
 #include "emu.h"
+#include "ini/ini.h"
+#include <stdlib.h>
+#include <string.h>
 
+PEMU_SETTINGS EmuSettings;
+
+void EmuInit(void) {
+    EmuSettings = malloc(sizeof(EMU_SETTINGS));
+    memset(EmuSettings, 0, sizeof(EMU_SETTINGS));
+    
+    PINI_FILE IniFile = IniOpenFile("emu.ini");
+   
+    if (!IniFile) {
+        IniFile = IniCreateFile("emu.ini");
+        IniCreateField(IniFile, "FdiskBin", "hdd.bin");
+        IniCreateField(IniFile, "FdiskSize", "16777216");
+        IniCreateField(IniFile, "EmuPhysRam", "16777216");
+        // 16mb
+        
+        EmuSettings->DriveMaxSpace = 16777216;
+        EmuSettings->MemoryToAllocate = 16777216;
+        strcpy(EmuSettings->DiskRaw, "hdd.bin");
+    } else {
+        PINI_ENTRY FdiskEntry = IniReadField(IniFile, 
+            "FdiskBin");
+        PINI_ENTRY FdSzeEntry = IniReadField(IniFile, 
+            "FdiskSize");
+        PINI_ENTRY RamSzEntry = IniReadField(IniFile, 
+            "EmuPhysRam");
+        
+        strcpy(EmuSettings->DiskRaw, FdiskEntry->Name);
+        EmuSettings->DriveMaxSpace = FdSzeEntry->Numerical;
+        EmuSettings->MemoryToAllocate = RamSzEntry->Numerical;
+        
+        IniCloseFile(IniFile);
+    }
+    
+    return;
+}
+
+void EmuShutdown(void) {
+    if (EmuSettings)
+        free(EmuSettings);
+    
+    return;
+}
